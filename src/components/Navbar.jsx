e79bcc52bedb07ae40f83f6718f70a2873eeefc5
Allyson Mitchem <allysonmitchem@gmail.com>
2023-07-20 17:08:39 -0600

    added custom image for site title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import React from "react"
 const Navbar = () => {
     return (
         <div id ="navbar">
-            <h2 id="siteName"> The Park Whisperer</h2>
+            <img id="navImage" src="https://res.cloudinary.com/fsa2/image/upload/v1689894261/portfolio%20site%20images/Project%20Images/parkwhisperer_i6gc1j.png" alt="icon"/>
+            {/* <h2 id="siteName"> The Park Whisperer</h2> */}
         </div>
     )
 
