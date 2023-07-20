3040b3d31422e213e16946fefb6597b3e138d467
Allyson Mitchem <allysonmitchem@gmail.com>
2023-07-20 15:24:16 -0600

    added favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Navbar = () => {
     return (
         <div id ="navbar">
-            <h2> I am navbar</h2>
+            <h2 id="siteName"> The Park Whisperer</h2>
         </div>
     )
 
