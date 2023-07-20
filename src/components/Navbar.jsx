1abae5b0ce12cd27175c6fed9b82664461a8fd4b
Allyson Mitchem <allysonmitchem@gmail.com>
2023-07-20 17:15:13 -0600

    changed favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Navbar = () => {
     return (
         <div id ="navbar">
-            <img id="navImage" src="https://res.cloudinary.com/fsa2/image/upload/v1689894261/portfolio%20site%20images/Project%20Images/parkwhisperer_i6gc1j.png" alt="icon"/>
+            <img id="navImage" src="https://res.cloudinary.com/fsa2/image/upload/v1689894856/portfolio%20site%20images/Project%20Images/favIcon2_ctmnph.png" alt="icon"/>
             {/* <h2 id="siteName"> The Park Whisperer</h2> */}
         </div>
     )
