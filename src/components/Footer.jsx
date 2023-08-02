c5c289fd8a7eb80877a01ed991e2612586958da5
Allyson Mitchem <allysonmitchem@gmail.com>
2023-08-02 11:37:36 -0600

    changed top icon/colors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <div id="footerContainer">
         <div id ="footer">
+        {/* <img id="footerImage" src="https://res.cloudinary.com/fsa2/image/upload/v1690996807/portfolio%20site%20images/Project%20Images/darkParkIcon_knmw8e.png" alt="icon"/> */}
             <hr class="footer-hr"/>
             <h2 class="resourcesTitle"> Resources</h2>
 
