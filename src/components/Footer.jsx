4fa3cf2bf925a5365882d95ad4fd365835142894
Allyson Mitchem <allysonmitchem@gmail.com>
2023-08-02 12:43:46 -0600

    added link to api developer resources

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,13 @@ const Footer = () => {
         <div id ="footer">
             {/* <hr class="footer-hr"/> */}
             <div class="linkContainer">
-            <h2 class="resourcesTitle"> Resources</h2>
+            {/* <h2 class="resourcesTitle">RESOURCES</h2> */}
+            <div class="links">
+                <h2 class="resourcesTitle">LINKS</h2>
+                <a class="siteLinks" href="https://www.nps.gov/subjects/developer/index.htm"  target="_blank">DEVELOPER RESOURCES</a>
+            </div>
+            <h2 class="resourcesTitle">CONTACT US</h2>
+           
             </div>
 
         <img id="footerImage" src="https://res.cloudinary.com/fsa2/image/upload/v1690999680/portfolio%20site%20images/Project%20Images/fire_ba5i35.png" alt="icon"/>
