caaa0095b2dfaaa1e25e870659575f9efbcec638
Allyson Mitchem <allysonmitchem@gmail.com>
2023-08-02 12:12:55 -0600

    changed footer image/ colors of navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,12 @@ const Footer = () => {
     return (
         <div id="footerContainer">
         <div id ="footer">
-        {/* <img id="footerImage" src="https://res.cloudinary.com/fsa2/image/upload/v1690996807/portfolio%20site%20images/Project%20Images/darkParkIcon_knmw8e.png" alt="icon"/> */}
-            <hr class="footer-hr"/>
+            {/* <hr class="footer-hr"/> */}
+            <div class="linkContainer">
             <h2 class="resourcesTitle"> Resources</h2>
+            </div>
+
+        <img id="footerImage" src="https://res.cloudinary.com/fsa2/image/upload/v1690999680/portfolio%20site%20images/Project%20Images/fire_ba5i35.png" alt="icon"/>
 
         </div>
         </div>
