b1294056ddf6463ef0c25fa58815434136d7b082
Allyson Mitchem <allysonmitchem@gmail.com>
2023-08-03 14:56:29 -0600

    created home component and added navlink to navbar for home

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import {Navbar, Footer} from './'
+import {Navbar, Footer, Home} from './'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 const Main = () => {
   return (
+    <Router>
     <div id="main">
     <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
     <Footer/>
   </div>
+  </Router>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react"
+import {NavLink} from "react-router-dom"
+import {Home} from './'
 
 const Navbar = () => {
     return (
         <div id ="navbar">
             <img id="navImage" src="https://res.cloudinary.com/fsa2/image/upload/v1690997495/portfolio%20site%20images/Project%20Images/hikerIcon_wjmumb.png" alt="icon"/>
-            {/* <h2 id="siteName"> The Park Whisperer</h2> */}
+           
+            <NavLink id="homeLink" to="/">Home</NavLink>
         </div>
     )
 
