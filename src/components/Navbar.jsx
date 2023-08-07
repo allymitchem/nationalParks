051a9694a72dd4621923229738554dacb9205cf9
Allyson Mitchem <allysonmitchem@gmail.com>
2023-08-07 14:10:41 -0600

    styling navlinks in navbar, changed background color of main, removed code for original background (topographic)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,10 @@ const Navbar = () => {
     return (
         <div id ="navbar">
             <img id="navImage" src="https://res.cloudinary.com/fsa2/image/upload/v1690997495/portfolio%20site%20images/Project%20Images/hikerIcon_wjmumb.png" alt="icon"/>
-           
-            <NavLink id="homeLink" to="/">Home</NavLink>
-            <NavLink id="parksLink" to="parks">Parks</NavLink>
+           <div class="navLinks">
+            <NavLink  id="homeLink"  to="/">Home</NavLink>
+            <NavLink  id="parksLink"  to="parks">Parks</NavLink>
+            </div>
         </div>
     )
 
