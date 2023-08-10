efcab8da217e78fe7e5102e8c65b9d9b08cfca89
Allyson Mitchem <allysonmitchem@gmail.com>
2023-08-10 17:19:31 -0600

    added api function for getting all parks data

--- src/apiFunctions/index.js
+++ src/apiFunctions/index.js
@@ -1,6 +1,38 @@
 const API_KEY = "MBgtPdBo8LmOB5HGbLZZjRqMUR9oQjfaTFBRCkc2";
 const BASE = "https://developer.nps.gov/api/v1/parks";
 
+export async function getAllParksData(){
+    try{
+        const response = await fetch(`${BASE}?api_key=${API_KEY}`)
+        if (!response.ok){
+            throw Error("Request failed.")
+        }
+        const data= await response.json()
+        console.log(data)
+        return data
+    } catch(error){
+        console.error("Error fetching parks data:", error)
+        return null
+    }
+}
+
+// async function testAllParks(){
+
+    
+//         const apiResponse = await getAllParksData()
+    
+//         if (apiResponse && apiResponse.data){
+           
+//             console.log("AllParksData:", apiResponse)
+//         } else
+//         console.log("No park data available.")
+//     }
+//     testAllParks();
+    
+    
+    
+    
+
 export async function getParkData(parkCode){
     try {
         const response = await fetch(`${BASE}?parkCode=${parkCode}&api_key=${API_KEY}`);
